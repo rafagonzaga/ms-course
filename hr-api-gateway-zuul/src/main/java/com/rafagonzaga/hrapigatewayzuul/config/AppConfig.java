4d85566eb6cddd52d2d83e65470497d60e3e68d2
Rafael Gonzaga <ralozaga@gmail.com>
2022-04-12 15:55:06 -0300

    Zuul authorization

---
+++ hr-api-gateway-zuul/src/main/java/com/rafagonzaga/hrapigatewayzuul/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.rafagonzaga.hrapigatewayzuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		// Configuração do segredo da chave secreta
+		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		return tokenConverter;
+	}
+	
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+	
+}
