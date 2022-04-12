73595e25b58270f6eeee0349ccb5538e9c029577
Rafael Gonzaga <ralozaga@gmail.com>
2022-04-12 16:58:39 -0300

    Security configs to config server

--- hr-oauth/src/main/java/com/rafagonzaga/hroauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/com/rafagonzaga/hroauth/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.rafagonzaga.hroauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,7 +16,14 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-
+	
+	@Value("${oauth.client.name}")
+	private String clientName;
+	
+	@Value("${oauth.client.secret}")
+	private String clientSecret;
+	
+	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 	
@@ -38,8 +46,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		// Método é para configurar com base nas credenciais do aplicativo e para configurar 
 		// com o tipo do grant_type
 		clients.inMemory()
-			.withClient("myappname123")
-			.secret(passwordEncoder.encode("myappsecret123"))
+			.withClient(clientName)
+			.secret(passwordEncoder.encode(clientSecret))
 			.scopes("read", "write")
 			.authorizedGrantTypes("password")
 			.accessTokenValiditySeconds(86400);
